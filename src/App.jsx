07ba4cd3854661eaf0e1a24dd6da14b57a90d217
kinjal verma <kinjal.v1710@gmail.com>
2026-07-08 01:23:13 +0530

    Fix import statement for Contact component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from "./components/About";
 import Skill from "./components/Skill";
 import Project from "./components/Projects";
 import Certificate from "./components/Certificate";
-import Contact from"./Components/Contact";
+import Contact from"./components/Contact";
 
 function App() {
   return (
